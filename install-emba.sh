0aa1ccaccd156819f2fa0bf3c9fc549e80497cca
Kees Jongenburger <62298192+keesj-exset@users.noreply.github.com>
2022-03-21 07:52:37 +0100

    use official git repository for emba

--- install-emba.sh
+++ install-emba.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
-#git clone https://github.com/e-m-b-a/emba.git
-git clone https://github.com/x7-labs/emba.git
+git clone https://github.com/e-m-b-a/emba.git
 cd emba
 sudo ./installer.sh -F
 
